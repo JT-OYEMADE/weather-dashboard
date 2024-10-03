f2ec24de6f68e9de9f0d031809b8d344ddb82f2a
Tomisin <oyemadeoluwatomisin75@gmail.com>
2024-10-03 17:53:45 +0100

    ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
